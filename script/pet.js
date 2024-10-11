6dd00d1b986cd98c14f88c2731554e0691657417
JubairAnjumRony <rony.ict.iu@gmail.com>
2024-10-11 16:57:00 +0600

    like section added

--- script/pet.js
+++ script/pet.js
@@ -1,5 +1,13 @@
 console.log("script section added successfully");
 
+const removeActiveClass =()=>{
+    const buttons = document.getElementsByClassName("category-btn");
+    console.log(buttons);
+    for(let btn of buttons){
+        btn.classList.remove("active");
+    }   
+}
+
 const loadCategories = ()=>{
     fetch('https://openapi.programming-hero.com/api/peddy/categories')
     .then((res)=>res.json())
@@ -13,23 +21,37 @@ const displayCategories = (categories)=>{
         console.log(item); 
 
         // create a button
-        const button =document.createElement("button");
-        button.classList = "btn btn-lg flex justify-center items-center gap-4 rounded-xl";
+        // const button =document.createElement("button");
+        const buttonContainer =document.createElement("div");
+        // button.classList = "btn btn-lg flex justify-center items-center gap-4 rounded-xl";
        
-        button.innerHTML =`
+        buttonContainer.innerHTML =`
+        <button id="btn-${item.category}" onclick ="loadCategoryPets('${item.category}')" class="btn btn-lg px-10  flex justify-center items-center gap-4 rounded-xl category-btn">
         <div>
           <img src="${item.category_icon}"/>
           </div>
           <div>
          <h3>${item.category}s</h3>
          </div>
+         </button>
         
         `;
-        categoryContainer.append(button);
+        categoryContainer.append(buttonContainer);
     });
 }
 
 const loadpets = ()=>{
+    //   const petcontainer = document.getElementById("loader");
+    //  petcontainer.innerHTML =`<span class="loading loading-bars loading-lg"></span>`
+    setTimeout(function(){
+        document.getElementById("loader").style.display="block";
+        showloader()
+    },2000)
+
+    const  showloader =()=>{
+        document.getElementById("loader").style.display="block";
+        console.log("5389345")
+    }
 
     fetch('https://openapi.programming-hero.com/api/peddy/pets')
     .then((res)=>res.json())
@@ -37,15 +59,112 @@ const loadpets = ()=>{
     .catch((error)=>console.log(error));
 }
 
+const loadDetails =async(petid)=>{
+    console.log(petid);
+    const uri =`https://openapi.programming-hero.com/api/peddy/pet/${petid}`;
+    const res = await fetch(uri);
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
+    displayDetails(data.petData);
+};
+
+const displayDetails = (pet)=>{
+   console.log(pet);
+   const detailContainer =document.getElementById("modal-content");
+   detailContainer.innerHTML =`
+   <img class="w-full  object-cover" src=${pet.image}/>
+    <div class="flex gap-5 border-b">
+
+     <div >
+         <h2 class="text-4xl font-extrabold pb-2"> ${pet.pet_name}</h2>
+           <div class="flex gap-1 items-center ">
+             <img class="h-[20px] w-[20px]" src="https://cdn-icons-png.flaticon.com/128/3388/3388614.png"/>
+             <p class="">Breed: ${pet.breed}</p>
+          
+           </div>
+           <div class="flex gap-1 items-center pb-2">
+             <img class="h-[20px] w-[20px]" src="https://cdn-icons-png.flaticon.com/128/866/866954.png"/>
+             <p class="">gender: ${pet.gender}</p>
+          
+           </div>
+           <div class="flex gap-1 items-center pb-2">
+             <img class="h-[20px] w-[20px]" src="https://cdn-icons-png.flaticon.com/128/3388/3388614.png"/>
+             <p class="">vaccinated_status: ${pet.vaccinated_status}</p>
+          
+           </div>
+              
+     </div>
+     <div>
+    <div class="flex  gap-1 items-center pt-9">
+      <img class="h-[15px] w-[20px]" src="https://cdn-icons-png.flaticon.com/128/2948/2948088.png"/>
+             <p class="">Birth: ${pet.date_of_birth.split("-")[0]}</p>
+
+    </div>
+    <div class="flex items-center gap-1 pb-2">
+      <img class="h-[15px] w-[20px]" src="https://cdn-icons-png.flaticon.com/128/2150/2150062.png"/>
+             <p class="">price: ${pet.price}</p>
+
+    </div>
+    </div>
+   
+</div>
+   
+   <div>
+    <h2 class="font-bold text-black pt-2">Details Information</h2>
+    <br>
+    <p>${pet.pet_details}</p>
+    </div>
+
+   `;
+   document.getElementById("custom_modal").showModal();
+};
+
+const likeDetails = (img) =>{
+    console.log(img);
+    const rightDiv =document.getElementById("anotherdiv");
+    const newDiv = document.createElement("div");
+    // rightDiv.classList=" grid grid-cols-2 shadow-md";
+    newDiv.classList ="col-span-1 ";
+    newDiv.innerHTML =`
+    <div>
+     <img src = "${img}" class="w-full max-h overflow-auto auto-rows-min object-cover" alt ="liked pet image"/>
+     </div>
+`;
+  rightDiv.append(newDiv);
+}
+
 const displaypets = (pets)=>{
     const petcontainer = document.getElementById("pets");
     // petcontainer.classList="grid grid-cols-4";
     // const maindiv =document.createElement('div');
-    // maindiv.classList =" grid col-sapn-3"
+    // maindiv.classList =" grid col-span-3"
     
+        // document.getElementById("loader").style.display="none";
+        // petcontainer.innerHTML = `
+        //  showloader();
+        // `
+     
+    petcontainer.innerHTML ="";
+    
+    if(pets.length == 0){
+        petcontainer.classList.remove("grid");
+        petcontainer.innerHTML = `
+        <div class="min-h-[300px] flex flex-col gap-5 justify-center items-center">
+         <img src ="./assets/error.webp"/>
+
+         <h2 class ="text-center text-xl font-bold">NO Information Available</h2>
+         <p class="text-center items-center">Sorry we don't have birds right now.We will let you know when Birds will be available.Now at this moment we are out of stock</p>
+        
+        </div>`;
+    }
+    else{
+        petcontainer.classList.add("grid");
+    }
+
      
     pets.forEach((item)=>{
-        console.log(item);
+        console.log((item));
   
     const card =document.createElement('div');
     card.classList="card  col-span-1 ";
@@ -56,16 +175,34 @@ const displaypets = (pets)=>{
       alt="Shoes"
       class="rounded-xl" />
   </figure>
-  <div class="card-body flex flex-col items-start text-left">    
-    <h2 class="card-title text-left">${item.pet_name}</h2>
-    <p class="text-left">Breed: ${item.breed}</p>
-    <P class="text-left">Birth:${(item.date_of_birth)} </p>
-    <P class="text-left">Gender:${item.gender} </p>
-    <P class="text-left">Price:${item.price} </p>
+  <div class="card-body flex flex-col items-start text-left">
+   <div class="border-b pb-4"> 
+        <h2 class="card-title text-left">${item.pet_name}</h2>   
+       <div class="flex gap-1 items-center ">
+             <img class="h-[20px] w-[20px]" src="https://cdn-icons-png.flaticon.com/128/3388/3388614.png"/>
+             <p class="">Breed: ${item.breed}</p>
+             </div>
+          <div class="flex items-center  gap-1 ">
+      <img class="h-[15px] w-[20px]" src="https://cdn-icons-png.flaticon.com/128/2948/2948088.png"/>
+             <p class="">Birth: ${item.date_of_birth}</p>
+
+           </div>
+           <div class="flex gap-1 items-center pb-2">
+             <img class="h-[20px] w-[20px]" src="https://cdn-icons-png.flaticon.com/128/866/866954.png"/>
+             <p class="">gender: ${item.gender}</p>
+          
+           </div>
+
+             <div class="flex gap-1 items-center pb-2">
+      <img class="h-[15px] w-[20px]" src="https://cdn-icons-png.flaticon.com/128/2150/2150062.png"/>
+             <p class="">price: ${item.price}</p>
+
+    </div>
+    </div>
     <div class="card-actions flex justify-around">
-    <button id ="like"class="btn"><img src="https://img.icons8.com/?size=32&id=15956&format=png"/></button>
-      <button id="adopt" class="btn">Adopt</button>
-      <button id ="details" class="btn ">Details</button>
+    <button id ="like" onclick = "likeDetails('${item.image}')" class="btn btn-sm"><img src="https://img.icons8.com/?size=32&id=15956&format=png"/></button>
+      <button id="adopt" class="btn btn-sm">Adopt</button>
+      <button onclick="loadDetails('${item.petId}')" class="btn btn-sm">Details</button>
     </div>
   </div>
     
@@ -76,15 +213,32 @@ const displaypets = (pets)=>{
 
 petcontainer.append(card);
 });
-const anotherDiv =document.getElementById("anotherdiv");
-    anotherDiv.classList ="  border-2 bg-red-500 ";
-    anotherDiv.innerHTML =`
- <div>
- </div>
-`;
 
-  anotherDiv.append();
+}
+//   anotherDiv.append();
 
+// };
+//  const showloader = ()=>{
+//     console.log("2 seconds gone");
+//     document.getElementById("loader").style.display="none";
+
+//  }
+
+// show pets according to category
+const loadCategoryPets =(name)=>{
+   
+
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${name}`)
+    .then((res)=>res.json())
+
+    .then((data)=>{
+        
+        removeActiveClass();
+        const activeBtn = document.getElementById(`btn-${name}`);
+        activeBtn.classList.add("active");
+        displaypets(data.data)
+    })
+    .catch((error)=>console.log(error));
 };
 loadCategories();
 loadpets();
